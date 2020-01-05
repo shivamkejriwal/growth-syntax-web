f4c61b6d8cce3fc9098c420fe464530431ab4d5f
Shivam Kejriwal <shivamkejriwal@gmail.com>
2020-01-04 20:25:41 -0800

    Advanced sesssion handling

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,5 +4,5 @@ export const SIGN_IN = '/signin';
 // export const SIGN_OUT = '/signout';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
-// export const ADMIN = '/admin';
+export const ADMIN = '/admin';
 export const PASSWORD_FORGET = '/pw-forget';
